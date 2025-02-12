9e42c893d25936faed9222af73ac261a8c8983a1
moximo <chenyi1@chagee.com>
2025-02-12 18:02:40 +0800

    fix aider

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -557,7 +557,7 @@ return {
       auto_manage_context = true, -- automatically manage buffer context
       default_bindings = false,    -- use default <leader>A keybindings
       debug = false,              -- enable debug logging
-      vim.api.nvim_set_keymap('n', '<leader>Ao', ':AiderOpen --no-auto-commits --vim --watch-files <CR>', {noremap = true, silent = true}),
+      vim.api.nvim_set_keymap('n', '<leader>Ao', ':AiderOpen --no-auto-commits --vim --watch-files --dark-mod <CR>', {noremap = true, silent = true}),
       vim.api.nvim_set_keymap('n', '<leader>Am', ':AiderAddModifiedFiles<CR>', {noremap = true, silent = true})
     },
   },
