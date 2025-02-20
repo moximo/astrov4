85f2401c35399f1f8c9b8c2423c14b048ca2fc44
moximo <chenyi1@chagee.com>
2025-02-20 15:50:36 +0800

    fix

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -582,7 +582,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    explorer = { enabled = true },
+    explorer = { enabled = false },
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
