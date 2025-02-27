112c5dfc51ffde59bb967e04ff0c7f00917e3307
moximo <chenyi1@chagee.com>
2025-02-27 11:07:55 +0800

    fix

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -724,5 +724,17 @@ return {
     })
   end,
 },
-
+{
+    -- ai 自动补全
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+            accept_suggestion = "<C-f>",
+            clear_suggestion = "<C-]>",
+            accept_word = "<C-a>",
+        },
+      })
+    end,
+},
 }
